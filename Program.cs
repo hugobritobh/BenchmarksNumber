3c931227dde01aabd4fc850acb4897d6f09291bd
Hugo de Brito Valadares Rodrigues Alves <hugo.alves@animaeducacao.com.br>
2020-12-03 20:15:07 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Running;
+using System;
+using System.Threading.Tasks;
+
+namespace Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<IsNumberBenchmark>();
+        }
+    }
+
+  
+}
+
